142ae0e1f71826c3249c490255f8693230b14872
duynguyen <duyxitrum@gmail.com>
2024-11-02 08:40:32 +0700

    completed tinh tong

--- Source.cpp
+++ Source.cpp
@@ -1,14 +1,22 @@
 #include <iostream>
 
 using namespace std;
-int tong(int a, int b);
+int tong(int a, int b){
+	return a+b;
+}
 int hieu(int a, int b);
 int tich(int a, int b);
 float thuong(int a, int b);
 int main()
-{
+{	
+	int x,y;
 	cout << "DAY LA CHUONG TRINH DE THUC HANH GIT";
-
+	cout << "Nhap x :"  << endl;
+	cin >>x;
+	cout << "Nhap y:" << endl;
+	cin >>y;
+	int tinhTong = tong(a+b);
+	cout <<"Tong la : " << tinhTong<<endl;
 	system("pause");
 	return 0;
 }
